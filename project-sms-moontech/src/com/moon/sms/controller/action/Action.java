f4060ac819e49c1b70044efa39ed355f47b9ae2e
OIKKac <roenw92jpw@gmail.com>
2020-01-27 18:22:40 +0900

    master

---
+++ project-sms-moontech/src/com/moon/sms/controller/action/Action.java
@@ -0,0 +1,20 @@
+package com.moon.sms.controller.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @date 2020.01.22
+ * @page 634~
+ * @author ACJ_NOTEBOOK1
+ *
+ */
+public interface Action {
+	
+	public void execute(HttpServletRequest request, HttpServletResponse responese)throws ServletException, IOException;
+	
+	
+}
